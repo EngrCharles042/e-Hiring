8631ff8a1a45c382fe8803a996584788db1df42d
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-11-06 16:15:33 +0100

    spring security and jwt configuration and setup, first commit

--- swiftselect/src/main/java/com/swiftselect/repositories/EmployerRepository.java
+++ swiftselect/src/main/java/com/swiftselect/repositories/EmployerRepository.java
@@ -3,7 +3,10 @@ package com.swiftselect.repositories;
 import com.swiftselect.domain.entities.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployerRepository extends JpaRepository<Employer, Long> {
+    Optional<Employer> findByWorkEmail(String workEmail);
     boolean existsByWorkEmail(String workEmail);
     boolean existsByNotificationEmail(String notificationEmail);
 }
