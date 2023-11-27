941c63a05da91369f1ef102a826fab90f41e930c
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-11-27 20:26:23 +0100

    implement chat feature for application

--- swiftselect/src/main/java/com/swiftselect/repositories/JobPostRepository.java
+++ swiftselect/src/main/java/com/swiftselect/repositories/JobPostRepository.java
@@ -3,6 +3,7 @@ package com.swiftselect.repositories;
 import com.swiftselect.domain.entities.employer.Employer;
 import com.swiftselect.domain.entities.jobpost.JobPost;
 import com.swiftselect.domain.enums.EmploymentType;
+import com.swiftselect.domain.enums.Industry;
 import com.swiftselect.domain.enums.JobType;
 import com.swiftselect.domain.enums.PayRate;
 import org.springframework.data.domain.Page;
