f75da5d2551284b4c5bcd4e03931585e432ad854
EngrCharles042 <obicharles94@gmail.com>
2023-12-05 16:07:52 +0100

    Job posts by employer

--- swiftselect/src/main/java/com/swiftselect/infrastructure/controllers/employercontrollers/EmployerController.java
+++ swiftselect/src/main/java/com/swiftselect/infrastructure/controllers/employercontrollers/EmployerController.java
@@ -7,7 +7,9 @@ import com.swiftselect.payload.request.authrequests.ResetPasswordRequest;
 import com.swiftselect.payload.response.APIResponse;
 import com.swiftselect.payload.response.employerresponse.EmployerResponse;
 import com.swiftselect.payload.response.employerresponse.EmployerResponsePage;
+import com.swiftselect.payload.response.jobpostresponse.JobPostResponse;
 import com.swiftselect.services.EmployerService;
+import com.swiftselect.services.JobPostService;
 import com.swiftselect.utils.AppConstants;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +19,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/employer")
 public class EmployerController {
     private final EmployerService employerService;
+    private final JobPostService jobPostService;
     private final ModelMapper mapper;
 
 
@@ -65,6 +70,10 @@ public class EmployerController {
                                                                 @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
                                                                 @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_PAGE_NO, required = false) String sortDir){
         return employerService.getAllEmployers(pageNo, pageSize, sortBy, sortDir);
+    }
 
+    @GetMapping("/job-posts")
+    public ResponseEntity<APIResponse<List<JobPostResponse>>> getAllJobPostsByEmployer() {
+        return jobPostService.findJobPostsByEmployer();
     }
 }
